589cbab590275ca0fc97c84a22ccbc9084ca6ea3
Stephen L Herbert Jr <8steve8@gmail.com>
2026-02-16 03:20:57 -0500

    ff

--- start.jl
+++ start.jl
@@ -157,27 +157,32 @@ function initme()
     matrices[1] = copy(originalmatrix)
     println("ok")
 
-    shapeidx = ones(UInt8, 13)
-    congruenceidx = ones(UInt8, 13)
+    shapeidx = ones(UInt8, 16)
+    congruenceidx = ones(UInt8, 16)
     shapes[2] = nextshapes(shapes[1][1])
     congruent_shapes = congruentshapes(shapes[1][1])
 
-    return
     shape = shapes[N][shapeidx[N]]
     congruent_shapes = Dict{UInt8,Tuple{Vararg{BitMatrix}}}()
     congruent_shapes[N] = congruentshapes(shape)
+    N += 1
+    shapes[N] = nextshapes(shape)
+    congruent_shapes[N] = congruentshapes(shapes[N][shapeidx[N]])
+    congruenceidx[N] = 1
+
+
+    t0 = time()
+    t1 = t0
 
     while N < 17
         #congruenceidx[N] += 1
-        if congruenceidx[N] > length(congruent_shapes)
+        if congruenceidx[N] > length(congruent_shapes[N])
             congruenceidx[N] = 1
             shapeidx[N] += 1
             if shapeidx[N] > length(shapes[N])
                 shapeidx[N] = 1
-                congruent_shapes[N] = congruentshapes(shapes[N][shapeidx[N]])
+                #congruent_shapes[N] = congruentshapes(shapes[N][shapeidx[N]])
                 N -= 1
-
-                N < 1 && break
                 congruenceidx[N] += 1
             end
             congruent_shapes[N] = congruentshapes(shapes[N][shapeidx[N]])
@@ -188,19 +193,20 @@ function initme()
 
         for row in max(placement[N][1], 1, max_row[N] - size(shape, 1) + 1):min(13 - size(shape, 1) + 1, min_row[N])
             for col in max(placement[N][2] + 1, 1, max_col[N] - size(shape, 2) + 1):min(13 - size(shape, 2) + 1, min_col[N])
+                Ncount = 0
                 for r in axes(shape, 1)
                     for c in axes(shape, 2)
-                        if shape[r, c] && (matrices[N][row+r-1, col+c-1] != 0) && (matrices[N][row+r-1, col+c-1] != N)
+                        if shape[r, c] && (matrices[N-1][row+r-1, col+c-1] == N)
+                            Ncount += 1
+                        elseif shape[r, c] && (matrices[N-1][row+r-1, col+c-1] != 0)
                             @goto does_not_fit
                         end
                     end
                 end
-
-                for p in originaldata[N]
-                    if !shape[p[1]-row+1, p[2]-col+1]
-                        @goto does_not_fit
-                    end
+                if Ncount != length(originaldata[N])
+                    @goto does_not_fit
                 end
+
 
 
                 placement[N] = (row, col)
@@ -213,10 +219,19 @@ function initme()
                         end
                     end
                 end
+                if time() - t1 > 100
+                    println("time = ", time() - t0, " seconds")
+                    println(N)
+                    println(shapeidx)
+                    println(congruenceidx)
+                    prettyprint(matrices[N])
+                    t1 = time()
+                end
+
 
                 N += 1
                 N > 16 && @goto end_of_program
-                congruenceidx[N] = 1
+                congruenceidx[N] = 0
                 shapeidx[N] = 1
                 shapes[N] = nextshapes(shape)
                 congruent_shapes[N] = congruentshapes(shapes[N][shapeidx[N]])
@@ -229,16 +244,11 @@ function initme()
 
         @label next_N
         placement[N] = (0, 0)
-
-
-
+        congruenceidx[N] += 1
 
     end
     @label end_of_program
     prettyprint(matrices[16])
-
-
-
 
 end
 
